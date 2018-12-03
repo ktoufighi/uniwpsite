a03492a222be1e6bc7577f37d4b8a4f36a238487
Kathy Toufighi <kathy.toufighi@gmail.com>
2018-12-03 13:52:13 -0500

    working with parent chid functions in navigation

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php get_header();
-
-  ?>
+<?php get_header(); ?>
   <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/library-hero.jpg') ?>);"></div>
     <div class="page-banner__content container t-center c-white">
@@ -102,6 +100,6 @@
 </div>
   <?php
 
-  get_footer();
+get_footer();
 
 ?>
