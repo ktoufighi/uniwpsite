e1d26368c9bb5450bba11f88fbc5b35611563456
Kathy Toufighi <kathy.toufighi@gmail.com>
2018-12-07 09:22:48 -0500

    custom queries for event lists

--- front-page.php
+++ front-page.php
@@ -15,28 +15,54 @@
         <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
         <?php
+          $today = date('Ymd');
           $homepageEvents = new WP_Query(array(
-            'posts_per_page' => 2,
+            // setting posts to -1 will return all post queries
+            'posts_per_page' => -1,
             'post_type' => 'event',
+            'orderby' => 'meta_value_num',
+            'meta_key' => 'date',
+            'order' => 'ASC',
+            // ordering the date passed events should not show, we use array to give the custom query more than one parameter
+            'meta_query' => array(
+              array(
+                //event date is our key
+                'key' => 'date',
+                // to today's date if its bigger or equal display it
+                'compare' => '>=',
+                // value is today's date
+                'value' => $today,
+                // the type being compared is a numeric value
+                'type' => 'numeric'
+              )
+            )
 
           ));
 
           while ($homepageEvents->have_posts()) {
             $homepageEvents->the_post(); ?>
             <div class="event-summary">
-              <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month"><?php the_time('M'); ?></span>
-                <span class="event-summary__day"><?php the_time('d'); ?></span>
+              <a class="event-summary__date t-center" href="#">
+                <span class="event-summary__month"><?php
+                  $eventDate = new DateTime(the_field('date'));
+                  echo $eventDate->format('M');
+                ?></span>
+                <span class="event-summary__day"><?php echo $eventDate->format('d')
+                ?></span>
               </a>
               <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                <p><?php if (has_excerpt()) {
+                  echo get_the_excerpt();
+                } else {
+                  echo wp_trim_words(get_the_content(), 18);
+                } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
               </div>
             </div>
           <?php }
         ?>
 
-        <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+        <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
 
       </div>
     </div>
@@ -59,7 +85,11 @@
               </a>
               <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                <p><?php if (has_excerpt()) {
+                  echo get_the_excerpt();
+                } else {
+                  echo wp_trim_words(get_the_content(), 18);
+                } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
               </div>
             </div>
             <?php } wp_reset_postdata();
@@ -80,7 +110,7 @@
       </div>
     </div>
   </div>
-  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_url('/images/apples.jpg') ?>);">
+  <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg') ?>);">
     <div class="hero-slider__interior container">
       <div class="hero-slider__overlay">
         <h2 class="headline headline--medium t-center">An Apple a Day</h2>
--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function university_files() {
-  wp_enqueue_script('main-unversity-js', get_theme_file_uri('/js/scripts.js'), NULL, 'microtime()', true);
+  wp_enqueue_script('main-unversity-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, 'microtime()', true);
   wp_enqueue_style('custom-google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
@@ -21,3 +21,23 @@ function university_features() {
 }
 
 add_action('after_setup_theme', 'university_features');
+
+
+function university_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+      array(
+        'key' => 'date',
+        'compare' => '>=',
+        'value' => $today,
+        'type' => 'numeric'
+      )
+    ));
+  }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
