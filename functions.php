9b8ae446e9d10b5fe211b2f45b2a67e7d36898a6
Kathy Toufighi <kathy.toufighi@gmail.com>
2018-12-17 12:40:49 -0500

    making the head banner area code more dry

--- functions.php
+++ functions.php
@@ -1,5 +1,29 @@
 <?php
 
+function pageBanner($args = NULL) {
+
+  // The logic for page banner code lives here
+  if(!$args['title']) {
+    $args['title'] = get_the_title();
+  }
+
+  if(!$args['subtitle']) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner'] ?>); "></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
+      </div>
+    </div>
+  </div>
+<?php }
+
+
+
 function university_files() {
   wp_enqueue_script('main-unversity-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, 'microtime()', true);
   wp_enqueue_style('custom-google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -15,6 +39,7 @@ function university_features() {
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
   add_image_size('professorImage', 300, 195, true);
+  add_image_size('pageBanner', 1500, 350, true);
   register_nav_menu('headerMenuLocation', 'Header Menu Location');
   register_nav_menu('footerLocationOne', 'Footer Location One');
   register_nav_menu('footerLocationTwo', 'Footer Location Two');
